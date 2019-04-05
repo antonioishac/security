531a68a80c670743a64a777d28f7b95ca620f994
Antonio Ishac <antonioishac@gmail.com>
2019-04-04 23:14:24 -0300

    Projeto security

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_usuarios_e_permissoes.sql
@@ -0,0 +1,25 @@
+CREATE TABLE usuario (
+	codigo BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	senha VARCHAR(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE permissao (
+	codigo BIGINT(20) PRIMARY KEY,
+	descricao VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE usuario_permissao (
+	codigo_usuario BIGINT(20) NOT NULL,
+	codigo_permissao BIGINT(20) NOT NULL,
+	PRIMARY KEY (codigo_usuario, codigo_permissao),
+	FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
+	FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Antonio', 'aishac@gmail.com', '$2a$10$Dj9/P8v4oPieMX26iMlVfugGsOYUgX7rDynYiCR7NVhHrvrPhtW12');
+
+
+INSERT INTO permissao (codigo, descricao) values (1, 'ROLE_CADASTRAR_CATEGORIA');
+INSERT INTO permissao (codigo, descricao) values (2, 'ROLE_PESQUISAR_CATEGORIA');
