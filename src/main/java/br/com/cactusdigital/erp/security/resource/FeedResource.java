5944ed17377e942cb94f85e7f9b78d68c41e1907
Antonio Ishac <antonioishac@gmail.com>
2019-04-14 23:38:11 -0300

    Ajustando

--- src/main/java/br/com/cactusdigital/erp/security/resource/FeedResource.java
+++ src/main/java/br/com/cactusdigital/erp/security/resource/FeedResource.java
@@ -1,5 +1,6 @@
 package br.com.cactusdigital.erp.security.resource;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cactusdigital.erp.security.model.Comments;
 import br.com.cactusdigital.erp.security.model.Feed;
+import br.com.cactusdigital.erp.security.repository.CommentsRepository;
 import br.com.cactusdigital.erp.security.repository.FeedRepository;
 
 @RestController
@@ -22,6 +26,9 @@ public class FeedResource {
 
 	@Autowired
 	private FeedRepository repository;
+	
+	@Autowired
+	private CommentsRepository commentsRepository;
 
 	@PostMapping
 	public ResponseEntity<Feed> criar(@RequestBody Feed feed) {
@@ -45,7 +52,15 @@ public class FeedResource {
 
 		//Buscar o feed
 		Feed feed = repository.findOne(codigo);
-		feed.setIs_like("S");
+		
+		if (feed.getIs_like().equals("S")) {
+			Long contadorUsuario = feed.getLikeCount() -1L;
+			feed.setLikeCount(contadorUsuario);
+			feed.setIs_like("N");
+		} else if (feed.getIs_like().equals("N") || feed.getIs_like() == null) {
+			feed.setLikeCount( ( feed.getLikeCount() == null ? 0 : feed.getLikeCount() ) + 1L);
+			feed.setIs_like("S");
+		}
 		Feed salvar = repository.save(feed);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(salvar);
@@ -55,6 +70,16 @@ public class FeedResource {
 	public ResponseEntity<Feed> desLike(Long codigo) {
 		repository.delete(codigo);
 		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	@PostMapping("/comments")
+	public ResponseEntity<Comments> addComments(@RequestBody Comments comments) {
+		comments.setDataComment(LocalDate.now());
+		comments.setCodigoPhoto(1L);
+		
+		Comments commentsSalvo = commentsRepository.save(comments);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(commentsSalvo);
 	}
 
 }
